088d1d0bd5bec684f6a9d0450e511afd9815dc2a
Anh Tuan <tuandgtl7@gmail.com>
2023-03-09 15:03:45 +0700

    create typeScrip project

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import {
+  BrowserRouter as Router,
+  // Switch,
+  Route,
+  Routes,
+} from "react-router-dom";
+import "./App.css";
+import { Fragment } from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <h1>Chào anh em</h1>
+        {/* <Routes>
+          {publicRoutes.map((route, index) => {
+            let Layout = DefaulLayout;
+            if (route.layout) {
+              Layout = route.layout;
+            } else if (route.layout === null) {
+              Layout = Fragment;
+            }
+            // hiển thị các component theo đường path
+            const Page = route.component;
+            return (
+              <Route
+                key={index}
+                path={route.path}
+                element={
+                  <Layout>
+                    <Page />
+                  </Layout>
+                }
+              />
+            );
+          })}
+        </Routes> */}
+      </div>
+    </Router>
   );
 }
 
